9c9ce56922f7e8695418a8ad759dce9aecb4b8aa
hsu-git <hsu080206@gmail.com>
2025-04-17 10:37:56 +0900

    인덱스 페이지 완성

--- src/main/java/org/example/bootdiary/controller/MainController.java
+++ src/main/java/org/example/bootdiary/controller/MainController.java
@@ -1,7 +1,17 @@
 package org.example.bootdiary.controller;
 
+import org.example.bootdiary.model.entity.Article;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("title", "환영합니다");
+        model.addAttribute("message", "어서오세요");
+        model.addAttribute("frontImage", "/assets/red-panda.jpg");
+        return "index";
+    }
 }
