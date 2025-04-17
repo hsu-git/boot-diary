bf63f5fb3b598b681cf2f663813f2f8a15f84458
hsu-git <hsu080206@gmail.com>
2025-04-17 14:17:57 +0900

    RequiredArgsConstructor

--- src/main/java/org/example/bootdiary/service/ArticleServiceImpl.java
+++ src/main/java/org/example/bootdiary/service/ArticleServiceImpl.java
@@ -1,9 +1,8 @@
 package org.example.bootdiary.service;
 
 import jakarta.transaction.Transactional;
-import org.apache.coyote.BadRequestException;
+import lombok.RequiredArgsConstructor;
 import org.example.bootdiary.model.entity.Article;
-import org.example.bootdiary.model.form.ArticleForm;
 import org.example.bootdiary.model.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +11,9 @@ import java.util.List;
 // Component
 @Service // Spring
 @Transactional
+@RequiredArgsConstructor
 public class ArticleServiceImpl implements ArticleService {
     private final ArticleRepository articleRepository;
-
-    public ArticleServiceImpl(ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
-    }
 
     @Override
     public List<Article> findAll() {
